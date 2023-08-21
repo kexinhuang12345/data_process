c27405a7f33ad6e993ff8d3bf34dc79f832bc3d4
Tianfan Fu <tianfanfu@eduroamprvnat-172-16-59-77.near.illinois.edu>
2023-08-21 11:54:14 -0500

    add trial date into trial outcome prediction

--- data_process/trialoutcome.py
+++ data_process/trialoutcome.py
@@ -1,9 +1,18 @@
 import csv 
 
+date_file = 'nctid_date.txt'
 raw_file = "raw_data.csv"
 phase1_file = 'phase1.tab'
 phase2_file = 'phase2.tab'
 phase3_file = 'phase3.tab' 
+
+from collections import defaultdict
+nctid2date = defaultdict(lambda x:('NA', 'NA'))
+with open(date_file, 'r') as fin:
+	lines = fin.readlines()
+	for line in lines:
+		line = line.strip().split('\t')
+		nctid2date[line[0]] = line[1], line[2]
 
 with open(raw_file, 'r') as csvfile:
 	reader = list(csv.reader(csvfile, delimiter=','))
@@ -25,8 +34,11 @@ paired_data = [(phase1_file, phase1_lines), (phase2_file, phase2_lines), (phase3
 for file, lines in paired_data:
 	with open(file, 'w', newline='') as csvfile:
 	    writer = csv.writer(csvfile, delimiter='\t')
-	    writer.writerow(['drug_molecules', 'icdcodes', 'eligibility_criteria', 'Y'])
+	    writer.writerow(['nctid', 'start_date', 'complete_date', 'drug_molecules', 'icdcodes', 'eligibility_criteria', 'Y'])
 	    for i,line in enumerate(lines):
+	    	nctid = line[0]
+	    	start_date = nctid2date[nctid][0]
+	    	complete_date = nctid2date[nctid][1]
 	    	drug = line[8][2:-2].split("', '")
 	    	drug = '__'.join(drug)
 	    	label = line[3]
@@ -37,7 +49,7 @@ for file, lines in paired_data:
 	    	criteria = ' '.join(criteria.split('\n'))
 	    	for t in range(5):
 	    		criteria = ' '.join(criteria.split('  ')) #### remove multiple spaces. 
-	    	writer.writerow([drug, icdcodes, criteria, label])
+	    	writer.writerow([nctid, start_date, complete_date, drug, icdcodes, criteria, label])
 	    	# writer.writerow([str(i), str(i), str(i), drug, icdcodes, criteria, label])
 
 
